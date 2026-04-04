50e38c5a3a10105db047f9e354d7dcea2295a582
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-04-04 10:17:19 +0530

    refactor the code

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.db.mongo import connect_to_mongo, close_mongo_connection
+from app.database.mongodb import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
 
