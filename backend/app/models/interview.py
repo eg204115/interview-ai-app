5add98cf20c22eebacf5736b33ed9f3e0ee9765e
Nethmi Poornima <nethmipoornima2020@gmail.com>
2026-04-04 08:22:25 +0530

    update models with enums

--- backend/app/models/interview.py
+++ backend/app/models/interview.py
@@ -4,13 +4,15 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+from backend.app.core.enums import DifficultyLevel
+
 class Interview(BaseModel):
     interviewId:  str = str(uuid.uuid4())
     userId: str = str(uuid.uuid4())
     title: str
     description: Optional[str]
     jobPosition: str
-    difficulty: str
+    difficulty: DifficultyLevel
     startTime: Optional[datetime]
     endTime: Optional[datetime]
     status: str
--- backend/app/models/question.py
+++ backend/app/models/question.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 import uuid
 
+from backend.app.core.enums import DifficultyLevel
+
 class Question(BaseModel):
     questionId: str = str(uuid.uuid4())
     interviewId: str = str(uuid.uuid4())
     questionText: str
-    questionType: str
+    questionType: DifficultyLevel
     sequenceNumber: int
     timeLimit: int
     difficulty: str
